adc6509611fde915dbaea275857eb2056d74773b
ca081105@gmail.com <ca081105@gmail.com>
2024-03-20 17:06:21 +0900

    a

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        /*
+        int number = in.nextInt();
+        System.out.println("number: " + number);
+
+        String name = in.nextLine();
+        System.out.println("name: " + name);
+
+        String an = in.next();
+        System.out.println("an: " + an);
+        */
+
+        /*
+        int a = in.nextInt();
+        int b = in.nextInt();
+        if(a > b){
+            System.out.println("a가 더 큽니다.");
+        }
+        */
+
+
+        for(int i = 0; i < 5; i++){
+            System.out.println(i);
+        }
+
+
+
+
+    }
+}
